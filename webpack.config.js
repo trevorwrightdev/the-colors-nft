234079079c1e73b0ac98215cf61ecc71a3e15573
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-14 14:40:47 -0700

    Changed webapck index

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     devServer: {
-        historyApiFallback: true,
-        contentBase: './',
-        hot: true
+        historyApiFallback: {
+            index: './index.html'
+        }
     },
 }
 
