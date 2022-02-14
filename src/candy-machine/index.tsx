a21e3cbac28b806be3f3cd97f936216f9abd9750
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-14 12:45:48 -0700

    Made it work

--- src/candy-machine/index.tsx
+++ src/candy-machine/index.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './styles/index.css';
-import App from './components/App';
-import CandyMachine from './candy-machine/App'
+import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />}/>
-        <Route path='/mint' element={<CandyMachine />}/>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
